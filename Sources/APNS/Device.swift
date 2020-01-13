eb060acf82ef49adef370561da8634dfc020b63e
Jeff Lett <jeffctown@gmail.com>
2020-01-12 21:03:39 -0500

    Adding bundle identifier.

--- Sources/APNS/Device.swift
+++ Sources/APNS/Device.swift
@@ -10,18 +10,20 @@ public final class Device: Codable, Equatable {
     
     public static func == (lhs: Device, rhs: Device) -> Bool {
         return lhs.id == rhs.id && lhs.type == rhs.type && lhs.token == rhs.token && lhs.environment
-            == rhs.environment
+            == rhs.environment && lhs.bundleIdentifier == rhs.bundleIdentifier
     }
     
     public var id: Int?
     public var type: String
     public var token: String
     public var environment: APNS.Environment
+    public var bundleIdentifier: String
     
-    public init(type: String, token: String, environment: APNS.Environment = .release) {
+    public init(type: String, token: String, bundleIdentifier: String, environment: APNS.Environment = .release) {
         self.type = type
         self.token = token
         self.environment = environment
+        self.bundleIdentifier = bundleIdentifier
     }
 }
 
