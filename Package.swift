456e01b34b073d008d598fc6351e19785bbb68a0
Jeff Lett <jeffctown@gmail.com>
2020-12-13 07:26:00 -0500

    Removing vapor and fluent

--- Package.swift
+++ Package.swift
@@ -7,21 +7,9 @@ let package = Package(
     name: "APNS",
     products: [
         .library(name: "APNS", targets: ["APNS"]),
-        .library(name: "APNSFluent", targets: ["APNS", "APNSFluent"]),
-        .library(name: "APNSVapor", targets: ["APNS","APNSFluent","APNSVapor"]),
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.0.0")),
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from: "3.0.0")),
     ],
     targets: [
         .target(name: "APNS"),
         .testTarget(name: "APNSTests", dependencies: ["APNS"]),
-        .target(name: "APNSFluent", dependencies: ["APNS", "FluentSQLite"]),
-        .testTarget(name: "APNSFluentTests", dependencies: ["APNSFluent"]),
-        .target(name: "APNSVapor", dependencies: ["APNS","APNSFluent","Vapor"]),
-        .testTarget(name: "APNSVaporTests", dependencies: ["APNSVapor"]),
     ]
 )
